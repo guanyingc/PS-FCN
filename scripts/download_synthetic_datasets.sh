dfefe62bbfad885cb095484a31cdef7a983e88f6
GoYchen <490680694@qq.com>
2018-09-07 14:24:34 +0800

    Update: fix scripts/download_synthetic_datasets.sh

--- scripts/download_synthetic_datasets.sh
+++ scripts/download_synthetic_datasets.sh
@@ -2,7 +2,7 @@ mkdir -p data/datasets
 cd data/datasets
 
 # Download Synthetic dataset
-for dataset in "PS_Test_Sphere_Bunny.tgz" "PS_Blobby_Dataset.tgz"; do
+for dataset in "PS_Sculpture_Dataset.tgz" "PS_Blobby_Dataset.tgz"; do
     echo "Downloading $dataset"
     wget http://www.visionlab.cs.hku.hk/data/PS-FCN/datasets/$dataset
     tar -xvf $dataset
